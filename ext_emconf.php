5458a0a24ed5f12ab2373a872e5f09ed3f657139
Christian Pschorr <pschorr.christian@gmail.com>
2025-11-09 20:45:27 +0100

    Add TYPO3 extension configuration and initial SVG icon for Ps14 Component

---
+++ ext_emconf.php
@@ -0,0 +1,29 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "sitepackage".
+ *
+ * Auto generated on 20-08-2020 12:55.
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = [
+	'title' => 'Ps14 Component',
+	'description' => 'TYPO3 extension for provide basic (react) components',
+	'category' => 'frontend',
+	'author' => 'Christian Pschorr',
+	'author_email' => 'pschorr.christian@gmail.com',
+	'author_company' => '',
+	'state' => 'aplha',
+	'uploadfolder' => 0,
+	'clearCacheOnLoad' => 0,
+	'version' => '1.0.0',
+	'constraints' => [
+		'depends' => [],
+		'conflicts' => [],
+		'suggests' => [],
+	],
+];
